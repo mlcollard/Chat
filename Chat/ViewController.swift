3720fed59af43fd3d4742c9e67c3ae7e83c1c243
Michael L. Collard, Ph.D <collard@uakron.edu>
2018-04-30 08:01:43 -0400

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Michael Collard on 4/30/18.
+//  Copyright © 2018 Michael Collard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
